be51f5836800c52f62334d3a3ff9ef71ac107cd4
abril <abrilramar@gamil.com>
2020-10-06 17:57:38 -0500

    git R

--- server/config/config.js
+++ server/config/config.js
@@ -14,6 +14,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://USER1:khzoaWeom2MdiN09@cluster0.sxbsh.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI
 }
 process.env.URLDB = urlDB;
